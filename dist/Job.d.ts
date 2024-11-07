4399b9bc3b61384b422eda4ea83449befd2dd8f4
Jimmy <46508035+Jyrezo@users.noreply.github.com>
2024-11-07 13:05:19 -0500

    Delete dist directory

--- dist/Job.d.ts
+++
@@ -1,8 +0,0 @@
-declare class Job {
-    id: string;
-    expirationInSeconds: number;
-    category: number;
-    cores: number;
-    constructor(id: string, expirationInSeconds?: number, category?: number, cores?: number);
-}
-export default Job;
